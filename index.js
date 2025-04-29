fe1fcb523b30eb326a6ac18a43d47f89146be563
Robson Alves <contato@robsonalves.dev.br>
2025-04-29 08:46:08 -0300

    chore: initialize package.json for passport-oauth2-client with dependencies

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+
+const app = express();
+const OAuth2Strategy = require("passport-oauth2");
+
+app.use(
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: true },
+	}),
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.use(
+	new OAuth2Strategy(
+		{
+			authorizationURL:
+				"http://localhost:8080/realms/ecommerce/protocol/openid-connect/auth",
+			tokenURL:
+				"http://localhost:8080/realms/ecommerce/protocol/openid-connect/token",
+			clientID: "app-payment-client-secret",
+			clientSecret: "V4ebDlZGTK7jsW9jOKnxWp2z2vpaqkI8",
+			callbackURL: "http://localhost:8000/auth/example/callback",
+			//pkce: true,
+			scope: "openid app-payment-scope",
+			//state: true,
+		},
+		(accessToken, refreshToken, profile, cb) => {
+			//User.findOrCreate({ exampleId: profile.id }, (err, user) =>
+			//	cb(err, user),
+			//);
+			cb(null, { accessToken, refreshToken, profile });
+		},
+	),
+);
+
+app.get("/auth/example", passport.authenticate("oauth2"));
+
+app.get(
+	"/auth/example/callback",
+	passport.authenticate("oauth2", { failureRedirect: "/login" }),
+	(req, res) => {
+		// Successful authentication, redirect home.
+		res.redirect("/");
+	},
+);
+
+app.listen(8000, () => {
+	console.log("Server is running on http://localhost:8000");
+});
